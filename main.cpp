72d42405523c92e8ec81277fc581e624800031df
RONALDEDUARDOED <82458164+ron3do@users.noreply.github.com>
2021-10-13 14:44:09 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int num1=0;  // se crea una variable y se le asigna el valor de 5.
+int num2=0; // se crea una variable y se le asigna el valor de 7.
+int result=0; // se crea una variable y se le asigna el valor de 0.
+int main() {
+  /*
+  se crea un programa para experimentear el uso de las operaciones básicas y la visualizacion de los resultados
+  */
+cout<<"digite un numero \n";
+cin>>num1;
+cout<<"digite un numero \n";
+cin>>num2;
+
+
+
+
+
+
+  result=num1+num2;
+  //cout<<result<<"\n";
+  result=num2-num1;
+  //cout<<result<<"\n";
+  result=num1*num2;
+  //cout<<result<<"\n";
+
+  result=num1+num2;
+
+  cout<<"El resultado de la suma "<<num1<<" + "<<num2<<" es: "<<result<<"\n";
+
+  result=num2-num1;
+  cout<<"El resultado de la resta "<<num1<<" - "<<num2<<" es: "<<result<<"\n"; 
+  result=num1*num2;
+
+    cout<<"El resultado de la multiplicacion "<<num1<<" * "<<num2<<" es: "<<result<<"\n";
+  cout<<result<<"\n";
+
+  
+}
+
+
+
+// for(int x=0;x<3;x++)
+// for(x;x<3;x++)
